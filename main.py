569ee231c8a97f7fd08e1ff7c234049192d36ace
Damotsoff <george.damotsov@gmail.com>
2026-01-03 17:17:32 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import uvicorn
+from fastapi import FastAPI
+from items_views import router as items_router
+from users.views import router as users_router
+
+
+app = FastAPI()
+app.include_router(items_router)
+app.include_router(users_router)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app="main:app", reload=True)
