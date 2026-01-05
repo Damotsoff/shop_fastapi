7b4a9eb34174daad5609da9fac74f40010e92f12
Damotsoff <george.damotsov@gmail.com>
2026-01-05 20:29:07 +0300

    refact settings

--- core/config.py
+++ core/config.py
@@ -1,13 +1,19 @@
 from pydantic_settings import BaseSettings
+from pydantic import BaseModel
 from pathlib import Path
 
 BASE_DIR = Path(__file__).parent.parent
+DB_PATH = BASE_DIR/"db.sqlite3"
+
+class DbSettings(BaseModel):
+    url: str = f"sqlite+aiosqlite:///{DB_PATH}"
+    echo: bool = True
 
 
 class Setting(BaseSettings):
     api_prefix: str = "/api/v1"
-    db_url: str = f"sqlite+aiosqlite:///{BASE_DIR}/db.sqlite3"
-    db_echo: bool = True
+    db: DbSettings = DbSettings()
+   
 
 
 settings = Setting()
