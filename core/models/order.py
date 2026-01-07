8d55d513836c9c97e8ff21a49c16d17c65072d64
Damotsoff <george.damotsov@gmail.com>
2026-01-07 16:14:33 +0300

    add order model and nigration

---
+++ core/models/order.py
@@ -0,0 +1,9 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import func
+from datetime import datetime
+from .base import Base
+
+
+class Order(Base):
+    promocode: Mapped[str | None] 
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now(),default=datetime.now,)
