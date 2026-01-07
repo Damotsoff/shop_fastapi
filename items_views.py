ad608dead081950edbadcc89f5fe24106f6b3c18
Damotsoff <george.damotsov@gmail.com>
2026-01-07 18:05:16 +0300

    delete file items_views.py

--- items_views.py
+++
@@ -1,9 +0,0 @@
-from typing import Annotated
-from fastapi import APIRouter, Path
-
-router = APIRouter(prefix="/items", tags=["items"])
-
-
-@router.get("/{item_id}/")
-async def get_item_by_id(item_id: Annotated[int, Path(ge=1, le=1000_000)]):
-    return {"id": item_id}
