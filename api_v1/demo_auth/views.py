d2e728d019a175cc48f62771a91793784574aff4
Damotsoff <george.damotsov@gmail.com>
2026-01-09 19:10:20 +0300

    style: improve code formatting and readability in views.py

--- api_v1/demo_auth/views.py
+++ api_v1/demo_auth/views.py
@@ -3,8 +3,7 @@ import uuid
 from time import time
 from typing import Annotated, Any
 
-from fastapi import (APIRouter, Cookie, Depends, Header, HTTPException,
-                     Response, status)
+from fastapi import APIRouter, Cookie, Depends, Header, HTTPException, Response, status
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
 router = APIRouter(prefix="/demo-auth", tags=["Demo Auth"])
@@ -72,46 +71,57 @@ async def demo_auth_some_http_header(
         "username": username,
     }
 
-COOKIES: dict[str,dict[str,Any]] = {}
+
+COOKIES: dict[str, dict[str, Any]] = {}
 COOKIE_SESSION_ID_KEY = "web-app-session-id"
+
 
 def generate_session_id() -> str:
     return uuid.uuid4().hex
 
 
-
-def get_session_data(session_id:str = Cookie(alias=COOKIE_SESSION_ID_KEY)) -> dict:
+def get_session_data(session_id: str = Cookie(alias=COOKIE_SESSION_ID_KEY)) -> dict:
     if session_id not in COOKIES:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="not authtenticated.",)
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="not authtenticated.",
+        )
     return COOKIES[session_id]
-
 
 
 @router.get("/login-cookie/")
 async def demo_auth_login_set_cookies(
     response: Response,
-    username: str = Depends(get_auth_user_username,),
+    username: str = Depends(
+        get_auth_user_username,
+    ),
 ):
     session_id = generate_session_id()
-    COOKIES[session_id] = {"username":username,"login_at": time()}
-    response.set_cookie(COOKIE_SESSION_ID_KEY,session_id)
+    COOKIES[session_id] = {"username": username, "login_at": time()}
+    response.set_cookie(COOKIE_SESSION_ID_KEY, session_id)
     return {
         "result": "ok",
     }
 
+
 @router.get("/check-cookie/")
-async def demo_auth_check_cookie(user_session_data:dict =  Depends(get_session_data)):
+async def demo_auth_check_cookie(user_session_data: dict = Depends(get_session_data)):
     username = user_session_data["username"]
-    return {"message":f"Hello, {username}",
-            **user_session_data,}
+    return {
+        "message": f"Hello, {username}",
+        **user_session_data,
+    }
 
 
 @router.get("/logout-cookie/")
 async def demo_auth_logout_cookie(
-    response:Response,
-    session_id:str = Cookie(alias=COOKIE_SESSION_ID_KEY),
-    user_session_data:dict =  Depends(get_session_data)):
+    response: Response,
+    session_id: str = Cookie(alias=COOKIE_SESSION_ID_KEY),
+    user_session_data: dict = Depends(get_session_data),
+):
     COOKIES.pop(session_id)
     response.delete_cookie(COOKIE_SESSION_ID_KEY)
     username = user_session_data["username"]
-    return {"message":f"Bye, {username}",}
+    return {
+        "message": f"Bye, {username}",
+    }
