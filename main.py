c8cb9b07d8cb4e44b8245f78ce570f6ce87f18e3
Damotsoff <george.damotsov@gmail.com>
2026-01-09 21:22:54 +0300

    fix: remove unnecessary blank line in main.py

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from api_v1 import router as router_v1
 from core.config import settings
 from users.views import router as users_router
 
-
 app = FastAPI()
 app.include_router(router=router_v1, prefix=settings.api_prefix)
 app.include_router(users_router)
