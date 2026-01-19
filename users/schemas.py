63a94ae4982f99fca950dfa113749b3ee205fb03
Damotsoff <george.damotsov@gmail.com>
2026-01-19 17:17:31 +0300

    feat: add user schemas for response

--- users/schemas.py
+++ users/schemas.py
@@ -2,8 +2,17 @@ from typing import Annotated
 
 from annotated_types import MaxLen, MinLen
 from pydantic import BaseModel, EmailStr
+from pydantic.config import ConfigDict
 
 
 class CreateUser(BaseModel):
     username: Annotated[str, MinLen(5), MaxLen(100)]
     email: EmailStr
+
+
+class UserSchema(BaseModel):
+    model_config = ConfigDict(strict=True)
+    username: str
+    password: bytes
+    email: EmailStr | None = None
+    active: bool = True
